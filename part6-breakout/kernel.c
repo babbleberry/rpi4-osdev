05fc8b47338a8d11d6cf2b7ceb6fa4bddc7b51ca
Adam Greenwood-Byrne <adam@greenwoodbyrne.com>
2020-07-15 22:31:21 +0100

    Reduced MARGIN to 30 in part6-breakout now we got config.txt right

--- part6-breakout/kernel.c
+++ part6-breakout/kernel.c
@@ -4,7 +4,7 @@
 // The screen
 #define WIDTH         1920
 #define HEIGHT        1080
-#define MARGIN        75
+#define MARGIN        30
 #define VIRTWIDTH     (WIDTH-(2*MARGIN))
 #define FONT_BPG      8
 
