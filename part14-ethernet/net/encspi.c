dde4667e378d4284052f81f77fc91ee1f5e34bef
Adam Greenwood-Byrne <adam@isometim.es>
2021-10-31 00:18:05 +0100

    Simplifying the encspi.c

--- part14-ethernet/net/encspi.c
+++ part14-ethernet/net/encspi.c
@@ -7,18 +7,14 @@ void ENC_SPI_Select(unsigned char truefalse) {
 
 void ENC_SPI_SendBuf(unsigned char *master2slave, unsigned char *slave2master, unsigned short bufferSize) {
     spi_chip_select(0);
-    HAL_Delay(1);
     spi_send_recv(master2slave, slave2master, bufferSize);
     spi_chip_select(1); // De-select the ENC
-    HAL_Delay(1);
 }
 
 void ENC_SPI_Send(unsigned char command) {
     spi_chip_select(0);
-    HAL_Delay(1);
     spi_send(&command, 1);
     spi_chip_select(1); // De-select the ENC
-    HAL_Delay(1);
 }
 
 void ENC_SPI_SendWithoutSelection(unsigned char command) {
