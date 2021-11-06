bc2ecc934d51335e2a556984603015d98a7ad475
Adam Greenwood-Byrne <adam@isometim.es>
2021-11-06 17:16:46 +0000

    Added part15-tcpip - same as part14-ethernet, but replies to pings now

---
+++ part15-tcpip/kernel/arp.c
@@ -0,0 +1,199 @@
+#include "../net/enc28j60.h"
+#include "../include/fb.h"
+#include "../tcpip/ip_arp_udp_tcp.h"
+
+ENC_HandleTypeDef handle;
+
+// Structure for Ethernet header
+
+typedef struct {
+   uint8_t DestAddrs[6];
+   uint8_t SrcAddrs[6];
+   uint16_t type;
+} EtherNetII;
+
+// Ethernet packet types
+
+#define ARPPACKET 0x0608
+#define IPPACKET  0x0008
+
+// Structure for an ARP Packet
+
+typedef struct {
+   EtherNetII eth;
+   uint16_t hardware;
+   uint16_t protocol;
+   uint8_t hardwareSize;
+   uint8_t protocolSize;
+   uint16_t opCode;
+   uint8_t senderMAC[6];
+   uint8_t senderIP[4];
+   uint8_t targetMAC[6];
+   uint8_t targetIP[4];
+} ARP;
+
+// ARP OpCodes
+
+#define ARPREPLY   0x0200
+#define ARPREQUEST 0x0100
+
+// ARP hardware types
+
+#define ETHERNET   0x0100
+
+// MAC address to be assigned to the ENC28J60
+
+uint8_t myMAC[6] = { 0xc0, 0xff, 0xee, 0xc0, 0xff, 0xee };
+
+// Router MAC is not known to start with, and requires an ARP reply to find out
+
+uint8_t routerMAC[6] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+
+// IP address to be assigned to the ENC28J60
+
+uint8_t deviceIP[4] = { 192, 168, 0, 66 };
+
+// IP Address of the router, whose hardware address we will find using the ARP request
+
+uint8_t routerIP[4] = { 192, 168, 0, 1 };
+
+// HELPER FUNCTIONS
+
+void *memset(void *dest, unsigned char val, unsigned short len)
+{
+    uint8_t *ptr = dest;
+    while (len-- > 0)
+       *ptr++ = val;
+    return dest;
+}
+
+void *memcpy(void *dest, const void *src, unsigned short len)
+{
+    uint8_t *d = dest;
+    const uint8_t *s = src;
+    while (len--)
+       *d++ = *s++;
+    return dest;
+}
+
+uint8_t memcmp(void *str1, void *str2, unsigned count)
+{
+    uint8_t *s1 = str1;
+    uint8_t *s2 = str2;
+
+    while (count-- > 0)
+    {
+       if (*s1++ != *s2++)
+          return s1[-1] < s2[-1] ? -1 : 1;
+    }
+
+    return 0;
+}
+
+// MAIN FUNCTIONS
+
+void SendArpPacket(uint8_t *targetIP, uint8_t *deviceMAC)
+{
+   /* Parameters:
+    *   targetIP - The target IP Address for the ARP request (the one whose hardware
+    *              address we want)
+    *   deviceMAC - The MAC address of the ENC28J60, i.e. the source MAC for the ARP
+    *               request
+   */
+
+   ARP arpPacket;
+
+   // The source of the packet will be the ENC28J60 MAC address
+   memcpy(arpPacket.eth.SrcAddrs, deviceMAC, 6);
+    
+   // The destination is broadcast - a MAC address of FF:FF:FF:FF:FF:FF */
+   memset(arpPacket.eth.DestAddrs, 0xFF, 6);
+    
+   arpPacket.eth.type = ARPPACKET;
+   arpPacket.hardware = ETHERNET;
+    
+   // We want an IP address resolved
+
+   arpPacket.protocol = IPPACKET;
+   arpPacket.hardwareSize = 0x06; // sizeof(deviceMAC);
+   arpPacket.protocolSize = 0x04; // sizeof(deviceIP);
+   arpPacket.opCode = ARPREQUEST;
+  
+   // Target MAC is set to 0 as it is unknown
+   memset(arpPacket.targetMAC, 0, 6);
+    
+   // Sender MAC is the ENC28J60's MAC address
+   memcpy(arpPacket.senderMAC, deviceMAC, 6);
+    
+   // The target IP is the IP address we want resolved
+   memcpy(arpPacket.targetIP, targetIP, 4);
+  
+   // Check if the last reply has come from an IP address that we want i.e. someone else is already using it
+   if (!memcmp(targetIP, deviceIP, 4)) {
+      // Yes, someone is using our IP so set the sender IP to 0.0.0.0
+      memset(arpPacket.senderIP, 0, 4);
+   } else {
+      // No, nobody is using our IP so we can use it confidently
+      memcpy(arpPacket.senderIP, deviceIP, 4);
+   }
+
+   // Send the packet
+
+   debugstr("Sending ARP request.");
+   debugcrlf();
+   enc28j60PacketSend(sizeof(ARP), &arpPacket);
+}
+
+void net_test(void)
+{
+   while (1) {
+      while (!ENC_GetReceivedFrame(&handle));
+  
+      uint16_t len = handle.RxFrameInfos.length;
+      uint8_t *buffer = (uint8_t *)handle.RxFrameInfos.buffer;
+      packetloop_arp_icmp_tcp(buffer, len);
+   }
+}
+
+void enc28j60PacketSend(unsigned short buflen, void *buffer) {
+   if (ENC_RestoreTXBuffer(&handle, buflen) == 0) {
+      ENC_WriteBuffer((unsigned char *) buffer, buflen);
+      handle.transmitLength = buflen;
+      ENC_Transmit(&handle);
+   }
+}
+
+void init_network(void)
+{
+   handle.Init.DuplexMode = ETH_MODE_HALFDUPLEX;
+   handle.Init.MACAddr = myMAC;
+   handle.Init.ChecksumMode = ETH_CHECKSUM_BY_HARDWARE;
+   handle.Init.InterruptEnableBits = EIE_LINKIE | EIE_PKTIE;
+
+   debugstr("Starting network up.");
+   debugcrlf();
+   if (!ENC_Start(&handle)) {
+      debugstr("Could not initialise network card.");
+   } else {
+      debugstr("Setting MAC address to C0:FF:EE:C0:FF:EE.");
+      debugcrlf();
+
+      ENC_SetMacAddr(&handle);
+
+      debugstr("Network card successfully initialised.");
+   }
+   debugcrlf();
+
+   debugstr("Waiting for ifup... ");
+   while (!(handle.LinkStatus & PHSTAT2_LSTAT)) ENC_IRQHandler(&handle);
+   debugstr("done.");
+   debugcrlf();
+
+   debugstr("Initialising the TCP stack... ");
+   init_udp_or_www_server(myMAC, deviceIP);
+   debugstr("done.");
+   debugcrlf();
+
+   // Re-enable global interrupts
+   ENC_EnableInterrupts(EIE_INTIE);
+}
---
+++ part15-tcpip/kernel/kernel.c
@@ -0,0 +1,176 @@
+#include "../include/fb.h"
+#include "../include/io.h"
+#include "../include/spi.h"
+#include "../include/multicore.h"
+#include "../net/enc28j60.h"
+#include "kernel.h"
+
+void initProgress(void)
+{
+    drawRect(0, 0, 301, 50, 0x0f, 0);
+    drawString(309, 21, "Core 0", 0x0f, 1);
+
+    drawRect(0, 60, 301, 110, 0x0f, 0);
+    drawString(309, 81, "Core 1", 0x0f, 1);
+
+    drawRect(0, 120, 301, 170, 0x0f, 0);
+    drawString(309, 141, "Timer 1", 0x0f, 1);
+
+    drawRect(0, 180, 301, 230, 0x0f, 0);
+    drawString(309, 201, "Timer 3", 0x0f, 1);
+}
+
+void drawProgress(unsigned int core, unsigned int val) {
+    unsigned char col = (core + 1) + ((core + 1) << 4);
+
+    // val should be 0-100
+    if (val == 0) drawRect(1, (60 * core) + 1, 300, (60 * core) + 49, 0x00, 1);
+    if (val > 0) drawRect(1, (60 * core) + 1, (val * 3), (60 * core) + 49, col, 1);
+}
+
+void core3_main(void) {
+    clear_core3();                // Only run once
+
+    // Test the network card
+
+    spi_init();
+    init_network();
+    net_test();
+
+    while(1);
+}
+
+void core0_main(void)
+{
+    unsigned int core0_val = 0;
+
+    while (core0_val <= 100) {
+       wait_msec(0x100000);
+       drawProgress(0, core0_val);
+       core0_val++;
+    }
+
+    debugstr("Core 0 done.");
+    debugcrlf();
+}
+
+void core1_main(void)
+{
+    unsigned int core1_val = 0;
+
+    clear_core1();                // Only run once
+
+    while (core1_val <= 100) {
+       wait_msec(0x3FFFF);
+       drawProgress(1, core1_val);
+       core1_val++;
+    }
+
+    debugstr("Core 1 done.");
+    debugcrlf();
+ 
+    while(1);
+}
+
+// TIMER FUNCTIONS
+
+const unsigned int timer1_int = CLOCKHZ;
+const unsigned int timer3_int = CLOCKHZ / 4;
+unsigned int timer1_val = 0;
+unsigned int timer3_val = 0;
+
+void timer_init() {
+    timer1_val = REGS_TIMER->counter_lo;
+    timer1_val += timer1_int;
+    REGS_TIMER->compare[1] = timer1_val;
+
+    timer3_val = REGS_TIMER->counter_lo;
+    timer3_val += timer3_int;
+    REGS_TIMER->compare[3] = timer3_val;
+}
+
+void handle_timer_1() {
+    timer1_val += timer1_int;
+    REGS_TIMER->compare[1] = timer1_val;
+    REGS_TIMER->control_status |= SYS_TIMER_IRQ_1;
+
+    unsigned int progval = timer1_val / timer1_int;
+    if (progval <= 100) {
+       drawProgress(2, progval);
+    } else {
+       debugstr("Timer 1 done.");
+       debugcrlf();
+    }
+}
+
+void handle_timer_3() {
+    timer3_val += timer3_int;
+    REGS_TIMER->compare[3] = timer3_val;
+    REGS_TIMER->control_status |= SYS_TIMER_IRQ_3;
+
+    unsigned int progval = timer3_val / timer3_int;
+    if (progval <= 100) drawProgress(3, progval);
+}
+
+unsigned long timer_get_ticks() {
+    unsigned int hi = REGS_TIMER->counter_hi;
+    unsigned int lo = REGS_TIMER->counter_lo;
+
+    //double check hi value didn't change after setting it...
+    if (hi != REGS_TIMER->counter_hi) {
+        hi = REGS_TIMER->counter_hi;
+        lo = REGS_TIMER->counter_lo;
+    }
+
+    return ((unsigned long)hi << 32) | lo;
+}
+
+void timer_sleep(unsigned int ms) {
+    unsigned long start = timer_get_ticks();
+
+    while(timer_get_ticks() < start + (ms * 1000));
+}
+
+void HAL_Delay(volatile unsigned int Delay) {
+    timer_sleep(Delay);
+}
+
+unsigned int HAL_GetTick(void) {
+    return timer_get_ticks();
+}
+
+void main(void)
+{
+    fb_init();
+
+    unsigned int i=0;
+    while (i++<30) debugcrlf();
+
+    initProgress();
+
+    // Kick it off on core 1
+
+    start_core1(core1_main);
+
+    // Kick off the timers
+
+    irq_init_vectors();
+    enable_interrupt_controller();
+    irq_enable();
+    timer_init();
+
+    // Kick it off on core 3
+
+    start_core3(core3_main);
+
+    // Kick it off on core 0
+
+    core0_main();
+
+    // Disable IRQs and loop endlessly
+
+    irq_disable();
+    disable_interrupt_controller();
+
+    while(1);
+}
