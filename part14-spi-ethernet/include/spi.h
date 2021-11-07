dfc89a4449aacff3bd0f9545a2fbb33b999c7e9f
Adam Greenwood-Byrne <adam@isometim.es>
2021-11-07 19:29:45 +0000

    Updating docs

--- part14-spi-ethernet/include/spi.h
+++ part14-spi-ethernet/include/spi.h
@@ -2,5 +2,4 @@ void spi_init();
 void spi_send_recv(unsigned char *sbuffer, unsigned char *rbuffer, unsigned int size);
 void spi_send(unsigned char *data, unsigned int size);
 void spi_recv(unsigned char *data, unsigned int size);
-void spi_send_no_selection(unsigned char command);
 void spi_chip_select(unsigned char chip_select);
