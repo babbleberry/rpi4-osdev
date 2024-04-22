d70738eea6e5949ccc459030617f6b45692ddfc2
babbleberry <adam@greenwoodbyrne.com>
2024-04-22 10:50:24 +0100

    Updated to add -mstrict-align and use Apple Silicon gcc

--- part10-multicore/multicore.h
+++ part10-multicore/multicore.h
@@ -1,7 +1,7 @@
-extern unsigned int spin_cpu0;
-extern unsigned int spin_cpu1;
-extern unsigned int spin_cpu2;
-extern unsigned int spin_cpu3;
+extern unsigned long spin_cpu0;
+extern unsigned long spin_cpu1;
+extern unsigned long spin_cpu2;
+extern unsigned long spin_cpu3;
 
 void start_core1(void (*func)(void));
 void start_core2(void (*func)(void));
